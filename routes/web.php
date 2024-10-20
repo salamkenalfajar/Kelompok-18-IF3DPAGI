0848da6dcb7f95540016a37afdd7e3543c3a8ed7
Satria3312301092 <muhammadsatriaadjisandi@gmail.com>
2024-10-20 19:20:45 +0700

    halaman view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\View;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('tes', function() {
+    return view('riski');
 });
