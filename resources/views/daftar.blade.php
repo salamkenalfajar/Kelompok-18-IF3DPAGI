59e50e8bca13b82242b1e1199a75d0880132bc5b
Jamal-crypto-coin <richkyahnmad@gmail.com>
2024-11-04 01:23:29 +0700

    Jamal-Frontend Login, daftar, dan Semua halaman admin

--- resources/views/daftar.blade.php
+++ resources/views/daftar.blade.php
@@ -29,8 +29,21 @@
                 </div>
             </div>
             <div class="flex flex-col justify-center -translate-x-64 gap-4 ">
-                <label class="text-center text-3xl font-semibold text-color-coklat1 underline underline-offset-8">Login</label>
-                <label class="input input-bordered flex items-center gap-4">
+                <label class="text-center text-3xl font-semibold text-color-coklat1 underline">Daftar</label>
+                <label class="input input-bordered flex items-center gap-2">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 16 16"
+                        fill="currentColor"
+                        class="h-4 w-4 opacity-70">
+                        <path
+                            d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                        <path
+                            d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+                    </svg>
+                    <input type="text" class="grow" placeholder="Email" />
+                </label>
+                <label class="input input-bordered flex items-center gap-2">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 16 16"
@@ -41,7 +54,7 @@
                     </svg>
                     <input type="text" class="grow" placeholder="Username" />
                 </label>
-                <label class="input input-bordered flex items-center gap-4">
+                <label class="input input-bordered flex items-center gap-2">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 16 16"
@@ -54,7 +67,7 @@
                     </svg>
                     <input type="password" class="grow" value="password" />
                 </label>
-                <button class="btn btn-wide bg-color-coklat1 text-white gap-4">Login</button>
+                <button class="btn btn-wide bg-color-coklat1 text-white">Daftar</button>
             </div>
 
             <img class="object-contain translate-x-[21rem] absolute " width="447.7" src="{{ asset('Icon/elipse.svg') }}">
--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,9 @@ Route::get('pengguna', function() {
 Route::get('dashboard', function() {
     return view('dashboard');
 });
+Route::get('daftar', function() {
+    return view('daftar');
+});
 Route::get('login', function() {
     return view('login');
 });
